d3388533e5bfdb225117ad48a1a9ebd7c15e815c
Gisela Mariano <gisela-mariano@hotmail.com>
2023-01-17 12:40:37 -0300

    refactor: changing names from 'schema' to 'serializer'

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,24 +1,24 @@
 import { Router } from 'express';
 import verifySchemaMiddleware from '../middlewares/verifySchema.mdlw';
-import createUserSchema from '../schemas/users/create.schema';
 import createUserController from '../controllers/users/createUser.ctrl';
-import loginUserSchema from '../schemas/users/login.schema';
 import loginUserController from '../controllers/users/loginUser.ctrl';
 import listUserProfileController from '../controllers/users/listUserProfile.ctrl';
 import verifyUserExistsMiddleware from '../middlewares/user/verifyUserExists.mdlw';
+import createUserSerializer from '../serializers/users/create.schema';
+import loginUserSerializer from '../serializers/users/login.schema';
 
 const userRoutes = Router();
 export const authRoutes = Router();
 
 userRoutes.post(
   '',
-  verifySchemaMiddleware(createUserSchema),
+  verifySchemaMiddleware(createUserSerializer),
   createUserController,
 );
 userRoutes.get('/:id', verifyUserExistsMiddleware, listUserProfileController);
 authRoutes.post(
   '',
-  verifySchemaMiddleware(loginUserSchema),
+  verifySchemaMiddleware(loginUserSerializer),
   loginUserController,
 );
 
